91f34bda57e78338634f9cf983f5c354110cd001
HarshVaishnav7295 <harshvaishnav@techtic.agency>
2023-08-17 15:03:59 +0530

    sync

--- src/dtos/SendNotificationToDevice.dto.ts
+++ src/dtos/SendNotificationToDevice.dto.ts
@@ -1,17 +1,16 @@
-import { IsString } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class SendNotificationToDeviceDto{
-    @IsString()
-    @ApiProperty({example:"2dr3rs23sds3"})
-    token:string
+export class SendNotificationToDeviceDto {
+  @IsString()
+  @ApiProperty({ example: '2dr3rs23sds3' })
+  token: string;
 
-    @IsString()
-    @ApiProperty({example:"Notification Demo"})
-    title:string
+  @IsString()
+  @ApiProperty({ example: 'Notification Demo' })
+  title: string;
 
-    @IsString()
-    @ApiProperty({example:"This is notification demo project"})
-    message:string
-
+  @IsString()
+  @ApiProperty({ example: 'This is notification demo project' })
+  message: string;
 }
--- src/dtos/SendNotificationToDeviceWithData.dto.ts
+++ src/dtos/SendNotificationToDeviceWithData.dto.ts
@@ -1,22 +1,24 @@
-import { IsString } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class SendNotificationToDeviceWithDataDto{
-    @IsString()
-    @ApiProperty({example:"2dr3rs23sds3"})
-    token:string
+export class SendNotificationToDeviceWithDataDto {
+  @IsString()
+  @ApiProperty({ example: '2dr3rs23sds3' })
+  token: string;
 
-    @IsString()
-    @ApiProperty({example:"Notification Demo"})
-    title:string
+  @IsString()
+  @ApiProperty({ example: 'Notification Demo' })
+  title: string;
 
-    @IsString()
-    @ApiProperty({example:"This is notification demo project"})
-    message:string
+  @IsString()
+  @ApiProperty({ example: 'This is notification demo project' })
+  message: string;
 
-    @ApiProperty({example:{
-        name:"Harsh",
-        email:"harshvaishnav@techtic.agency"
-    }})
-    data:Object
+  @ApiProperty({
+    example: {
+      name: 'Harsh',
+      email: 'harshvaishnav@techtic.agency',
+    },
+  })
+  data: Object;
 }
